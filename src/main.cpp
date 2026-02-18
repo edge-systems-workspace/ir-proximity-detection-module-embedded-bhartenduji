7e27ebf2eaad50ddf386e6b32f155921a53541fc
Bhartendu ji <bhartendu.ji_cs.aiml24@gla.ac.in>
2026-02-18 15:02:10 +0530

    Add pin definitions for IR sensor and LED

--- src/main.cpp
+++ src/main.cpp
@@ -10,3 +10,7 @@
  * @author Bhartendu Ji
  * @date 18 Feb 2026
  */
+
+// Pin Definitions
+int irPin = 7;     ///< IR sensor OUT pin connected to digital pin 7
+int ledPin = 13;   ///< LED connected to digital pin 13
